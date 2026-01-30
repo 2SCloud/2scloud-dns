04e63a7363de02dd4992e489f631493277c27ebd
onihilist <mashirooo.yt@gmail.com>
2026-01-30 13:27:37 +0100

    feat(cache): change the struct to handle better the cache records

--- src/dns/cache/mod.rs
+++ src/dns/cache/mod.rs
@@ -6,7 +6,7 @@ use crate::dns::q_class::DNSClass;
 
 // TODO: light LRU system
 // Should make a real LRU system for v2 (handle millions of entries)
-// Should link them in a HashMap (max_entries: 100_000)
+// Should link them in a HashMap (max_entries: 150_000)
 #[allow(unused)]
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub(crate) struct CacheKey {
